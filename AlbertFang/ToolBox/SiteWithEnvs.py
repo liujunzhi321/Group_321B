46dfb552475de16bfa3fad9f754eae4a6f9de087
Albert <1440563214@qq.com>
2021-03-31 22:10:26 +0800

    Update SiteWithEnvs.py

--- AlbertFang/ToolBox/SiteWithEnvs.py
+++ AlbertFang/ToolBox/SiteWithEnvs.py
@@ -89,51 +89,5 @@ def join_attribute_to_pt():
     lake_with_envs.to_file(outfilepath, encoding='utf-8')
     return
 
-def combine_data(): 
-    """
-    将现有的的DOC数据与之前提取的环境因子合并为同一个表格
-    """
-    base_dir        =  r'C:\Users\Administrator\Desktop'
-    lake_doc_join   = r'soil\LakeCARWithLakeName.shp'
-    env_filename    = r'WatershedEnvs/watershed_attribution.xlsx'
-    
-    doc_join_filepath = os.path.join(base_dir, lake_doc_join)
-    env_filepath      = os.path.join(base_dir, env_filename)
-    
-    doc_join  = gpd.read_file(doc_join_filepath)
-    env_data = pd.read_excel(env_filepath)
-    lnames = doc_join['lakeJoin'].values
-    env_data_join = env_data.loc[lnames] 
-    env_data_join['lakeJoin'] = lnames
-    
-    data_join = pd.merge(doc_join, env_data_join, on='lakeJoin', how='left')
-    
-    outfilepath = os.path.join(base_dir, r'WatershedEnvs\CARJoin_20210315\CARJoin.xlsx')
-    outfilepath_2 = os.path.join(base_dir, r'WatershedEnvs\CARJoin_20210315\CARJoin_2.xlsx')
-    print(data_join)
-    env_data_join.to_excel(outfilepath)
-    doc_join.to_excel(outfilepath_2)
-    return
-
-def lakeshp_envs():
-    """
-    将环境变量写入湖泊的shapefile数据中
-    """
-    
-    envs_fp = r'I:/Qinghai_Tibet_Plateau/LakeDOC/Data/WatershedEnvs/watershed_attributes.xlsx'
-    lake_fp = r'I:/Qinghai_Tibet_Plateau/LakeDOC/Data/lakesShapefiles_dissolve/lakesShapefiles_dissolve.shp'
-    output_fp = r'I:\Qinghai_Tibet_Plateau\LakeDOC\Data\lakesShapefiles_dissolve\lakeWithEnvs.shp'
-    envs_data = pd.read_excel(envs_fp)
-    lake_data = gpd.read_file(lake_fp)
-    lake_data = pd.concat([lake_data, envs_data], axis=1)
-    print(envs_data)
-    print(lake_data)
-    lake_data.to_file(output_fp, encoding='utf-8')
-    
-
 if __name__ == "__main__":
-    # mean_obs()
-    # is_point_in_lake()
-    # join_lakename_to_pt()
-    # combine_data()
     join_attribute_to_pt()
