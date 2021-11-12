6532cc09e7c4814747c2ed34b9ea1117da3d62de
albertfang0926@qq.com <alberfang0926@qq.com>
2021-11-13 07:17:11 +0800

    fix bug

--- AlbertFang/Mark1_LakeBasin/PreProcess/reproject.py
+++ AlbertFang/Mark1_LakeBasin/PreProcess/reproject.py
@@ -22,7 +22,7 @@ def transform_albers_rst(rst_fp, target_rst_fp):
     Return:
         None
     """
-    tp_fp = r'I:\Qinghai_Tibet_Plateau\青藏高原范围与界线数据\DBATP\DBATP\DBATP_Polygon.shp'
+    tp_fp = r'/share/home/liujunzhi/liujunzhi/Albert/Data/DBATP/DBATP_Polygon.shp'
     rst = rioxr.open_rasterio(rst_fp)
     rst_crs = rst.rio.crs
     tp_df = gpd.read_file(tp_fp).to_crs(rst_crs)
